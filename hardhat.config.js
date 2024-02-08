bbbcc315619a09d40a958e4ab2742ae427955e74
SamYusuf101 <samyusuf101@gomail.com>
2024-02-08 13:53:49 +0100

    created interfaces

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,84 +1,67 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
-
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
-  solidity: {
-    compilers: [
-      { version: "0.8.7" },
-      { version: "0.6.6" },
-      { version: "0.4.19" },
-    ],
-  },
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      // // If you want to do some forking, uncomment this
-      // forking: {
-      //   url: MAINNET_RPC_URL
-      // }
-      chainId: 31337,
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8"
+            },
+            {
+                version: "0.6.12"
+            },
+            {
+                version: "0.4.19"
+            }
+        ]
     },
-    localhost: {
-      chainId: 31337,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL
+            }
+        },
+        localhost: {
+            chainId: 31337
+        }
+        // goerli: {
+        //     url: GOERLI_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 5,
+        //     blockConfirmations: 6,
+        // },
     },
-    sepolia: {
-      chainId: 11155111,
-      blockConfirmations: 6,
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
-    },
-  },
 
-  contractSizer: {
-    runOnCompile: false,
-    only: ["Raffle"],
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    // etherscan: {
+    //     apiKey: ETHERSCAN_API_KEY
+    // },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
-    player: {
-      default: 1,
-    },
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.7",
-      },
-      {
-        version: "0.4.24",
-      },
-    ],
-  },
-  mocha: {
-    timeout: 300000, // 500 seconds max for running tests
-  },
-};
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        }
+    }
+}
